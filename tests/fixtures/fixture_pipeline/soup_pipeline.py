9084f85310a732c93dba38af0ba74afd3d3642b6
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2023-04-25 15:13:35 -0500

    integration test: pipeline with model download(!)
    note: as of writing this, the dlhub endpoint has been
    down due to funcx->globus compute growing pains, meaning
    this integration test hasn't yet run on an endpoint with
    an actual container build. ¯\_(ツ)_/¯

---
+++ tests/fixtures/fixture_pipeline/soup_pipeline.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from typing import List
+
+from garden_ai import GardenClient, Model, Pipeline, step
+
+client = GardenClient()
+
+
+class Soup:
+    def __init__(self, ingredients):
+        self.contents = [float(ingredient) for ingredient in ingredients]
+
+
+@step
+def split_peas(ps: List) -> List[tuple]:
+    return [(p / 2, p / 2) for p in ps]
+
+
+@step
+def make_soup(splits: List[tuple]) -> Soup:
+    return Soup(x for x, _ in splits)
+
+
+@step
+def rate_soup(
+    soup_sample: Soup,
+    model=Model("owenpriceskelly@uchicago.edu-toy-model/1"),
+) -> float:
+    """huh, wonder what this Model is for?"""
+
+    assert model.model is None  # lazy - not loaded yet
+    try:
+        model.predict(soup_sample)
+    except Exception:
+        assert model.model is not None  # loaded, whatever it was
+    return 10 / 10
+
+
+# the step functions will be composed in order by the pipeline:
+ALL_STEPS = (
+    split_peas,
+    make_soup,
+    rate_soup,
+)
+
+REQUIREMENTS_FILE = str((Path(__file__).parent / "requirements.txt").resolve())
+
+
+soup_edibility_predictor: Pipeline = client.create_pipeline(
+    title="soup edibility predictor",
+    steps=ALL_STEPS,
+    requirements_file=REQUIREMENTS_FILE,
+    authors=["Me"],
+    contributors=["You"],
+    description="Big Pea Data as in big peas, not like big data",
+    version="0.0.1",
+    year=2023,
+    tags=["you're it"],
+    uuid="c9df58fa-2df2-490e-849f-9e9a94265f8e",  # WARNING: DO NOT EDIT UUID
+)
--- tests/integrations/test_pipeline_registration.py
+++ tests/integrations/test_pipeline_registration.py
@@ -17,6 +17,11 @@ def do_not_set_mlflow_env_variables():
     pass
 
 
+@pytest.fixture
+def dlhub_endpoint():
+    return "86a47061-f3d9-44f0-90dc-56ddc642c000"  # real endpoint
+
+
 @pytest.mark.integration
 def test_register_pipeline():
     fixture_pipeline_path = get_fixture_file_path(
@@ -48,10 +53,13 @@ def test_run_registered_pipeline():
 
 
 @pytest.mark.integration
-def test_register_and_run_with_env_vars(do_not_set_mlflow_env_variables):
+def test_register_and_run_with_env_vars(
+    do_not_set_mlflow_env_variables, dlhub_endpoint
+):
     client = GardenClient()
     # register the pipeline
     pipeline_path = get_fixture_file_path("fixture_pipeline/env_vars_pipeline.py")
+    # NOTE: this pipeline just returns a copy of the os.environ dictionary
     pipeline = load_pipeline_from_python_file(pipeline_path)
     container_id = "3dc3170e-2cdc-4379-885d-435a0d85b581"
     client.register_pipeline(pipeline, container_id)
@@ -59,6 +67,7 @@ def test_register_and_run_with_env_vars(do_not_set_mlflow_env_variables):
     # load from client, setting mlflow env vars
     registered_pipeline = client.get_registered_pipeline(pipeline.uuid)
 
+    input_data = "i'm afraid of change"
     # set arbitrary other env vars
     test_var, test_val = (
         "GARDEN_TEST_ENV_VARIABLE",
@@ -66,9 +75,25 @@ def test_register_and_run_with_env_vars(do_not_set_mlflow_env_variables):
     )
     registered_pipeline._env_vars[test_var] = test_val
 
-    dlhub_endpoint = "86a47061-f3d9-44f0-90dc-56ddc642c000"  # real endpoint
-    input_data = "i'm afraid of change"
     results = registered_pipeline(input_data, endpoint=dlhub_endpoint)
     assert results[test_var] == test_val
     assert "MLFLOW_TRACKING_URI" in results
     assert "MLFLOW_TRACKING_TOKEN" in results
+
+
+@pytest.mark.integration
+def test_register_and_run_with_model(do_not_set_mlflow_env_variables, dlhub_endpoint):
+    client = GardenClient()
+    # register the pipeline locally
+    pipeline_path = get_fixture_file_path("fixture_pipeline/soup_pipeline.py")
+    # NOTE: this pipeline references a model, and raises an exception if model
+    # fails to lazy-download when model.predict() is called
+    pipeline = load_pipeline_from_python_file(pipeline_path)
+    container_id = "3dc3170e-2cdc-4379-885d-435a0d85b581"
+    client.register_pipeline(pipeline, container_id)
+
+    # load from client, setting mlflow env vars
+    registered_pipeline = client.get_registered_pipeline(pipeline.uuid)
+
+    input_soup = [1, 2, 3]
+    assert registered_pipeline(input_soup, endpoint=dlhub_endpoint) == (10 / 10)
