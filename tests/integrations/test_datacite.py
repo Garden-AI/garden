a2f0d8d2ca6dcffc6c24bab5fe749eea8882d735
isaac-darling <61167692+isaac-darling@users.noreply.github.com>
2023-06-12 15:57:43 -0500

    stop test from checking for non-existence of doi

--- tests/integrations/test_datacite.py
+++ tests/integrations/test_datacite.py
@@ -4,7 +4,6 @@ import pytest
 @pytest.mark.integration
 def test_auto_doi_garden(garden_client, garden_no_fields):
     garden = garden_no_fields
-    assert not garden.doi
     garden.authors = ["pytest"]
     garden.title = "DOI request test (Garden)"
     garden.doi = garden_client._mint_doi(garden)
@@ -14,7 +13,6 @@ def test_auto_doi_garden(garden_client, garden_no_fields):
 @pytest.mark.integration
 def test_auto_doi_pipelines(garden_client, pipeline_toy_example):
     pipe = pipeline_toy_example
-    assert not pipe.doi
     pipe.authors = ["pytest"]
     pipe.title = "DOI request test (Pipeline)"
     garden_client._mint_doi(pipe)
