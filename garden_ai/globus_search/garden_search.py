bb54efbeaf74ad9320420beaca2f2dd2c366f0b2
isaac-darling <61167692+isaac-darling@users.noreply.github.com>
2023-06-12 09:47:47 -0500

    remove uuid from gsearch integration and fix tests

--- garden_ai/globus_search/garden_search.py
+++ garden_ai/globus_search/garden_search.py
@@ -13,15 +13,15 @@ class RemoteGardenException(Exception):
     """Exception raised when a requested Garden cannot be found"""
 
 
-def get_remote_garden_by_uuid(
-    uuid: str, env_vars: dict, search_client: SearchClient
+def get_remote_garden_by_doi(
+    doi: str, env_vars: dict, search_client: SearchClient
 ) -> Garden:
     try:
-        res = search_client.get_subject(GARDEN_INDEX_UUID, uuid)
+        res = search_client.get_subject(GARDEN_INDEX_UUID, doi)
     except GlobusAPIError as e:
         if e.code == 404:
             raise RemoteGardenException(
-                f"Could not reach find garden with id {uuid}"
+                f"Could not reach find garden with id {doi}"
             ) from e
         else:
             raise RemoteGardenException(
@@ -39,35 +39,12 @@ def get_remote_garden_by_uuid(
     return garden
 
 
-def get_remote_garden_by_doi(
-    doi: str, env_vars: dict, search_client: SearchClient
-) -> Garden:
-    query = f'(doi: "{doi}")'
-    try:
-        res = search_client.search(GARDEN_INDEX_UUID, query, advanced=True)
-    except GlobusAPIError as e:
-        raise RemoteGardenException(f"Could not reach index {GARDEN_INDEX_UUID}") from e
-    try:
-        parsed_result = json.loads(res.text)
-        if parsed_result.get("count", 0) < 1:
-            raise RemoteGardenException(f"Could not find garden with doi {doi}")
-        garden_meta = parsed_result["gmeta"][0]["entries"][0]["content"]
-        garden = Garden(**garden_meta)
-    except (ValueError, KeyError, IndexError, ValidationError) as e:
-        raise RemoteGardenException(
-            f"Could not parse search response {res.text}"
-        ) from e
-    garden._env_vars = env_vars
-    garden._set_pipelines_from_remote_metadata(garden_meta["pipelines"])
-    return garden
-
-
 def publish_garden_metadata(
     garden: Garden, search_client: SearchClient
 ) -> GlobusHTTPResponse:
     garden_meta = json.loads(garden.expanded_json())
     gmeta_ingest = {
-        "subject": garden_meta["uuid"],
+        "subject": garden_meta["doi"],
         "visible_to": ["all_authenticated_users"],
         "content": garden_meta,
     }
--- tests/test_search.py
+++ tests/test_search.py
@@ -10,22 +10,12 @@ class MockSearchResult:
     text: str
 
 
-def test_get_by_doi_valid(mocker, valid_search_by_doi):
-    mock_search_client = mocker.MagicMock(SearchClient)
-    mock_search_client.search = mocker.Mock(
-        return_value=MockSearchResult(valid_search_by_doi)
-    )
-    garden = garden_search.get_remote_garden_by_doi("foo", {}, mock_search_client)
-    # Simple check that garden was populated with fields.
-    assert garden.year == "1863"  # Old timey Mendel year
-
-
-def test_get_by_uuid_valid(mocker, valid_search_by_subject):
+def test_get_by_doi_valid(mocker, valid_search_by_subject):
     mock_search_client = mocker.MagicMock(SearchClient)
     mock_search_client.get_subject = mocker.Mock(
         return_value=MockSearchResult(valid_search_by_subject)
     )
-    garden = garden_search.get_remote_garden_by_uuid("foo", {}, mock_search_client)
+    garden = garden_search.get_remote_garden_by_doi("foo", {}, mock_search_client)
     # Simple check that garden was populated with fields
     assert garden.year == "1863"  # Old timey Mendel year
 
