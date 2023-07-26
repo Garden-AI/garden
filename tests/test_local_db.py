f87f86e543a220c0ace36f9a9479c01f7824fd0e
isaac-darling <61167692+isaac-darling@users.noreply.github.com>
2023-07-26 16:15:23 -0500

    use fixed mlflow version

--- tests/test_local_db.py
+++ tests/test_local_db.py
@@ -57,33 +57,6 @@ def test_local_storage_model(mocker, database_with_model, second_draft_of_model)
 
 
 def test_local_cache(mocker, garden_client, pipeline_toy_example):
-    build_method = mocker.patch(
-        "garden_ai.client.build_container",
-        return_value="d1fc6d30-be1c-4ac4-a289-d87b27e84357",
-    )
-    # return value is the checksum for pip_dependencies=["tensorflow"] (implicitly also contains pandas<3 and mlflow-skinny==2.5.0)
-    mocker.patch(
-        "garden_ai.client._read_local_cache",
-        return_value={"1529529185": "04332b26-bc14-45e1-a296-482e27c72500"},
-    )
-    mocker.patch("garden_ai.client._write_local_cache", return_value=None)
-    mocker.patch(
-        "garden_ai.client.register_pipeline",
-        return_value="9f5688ac-424d-443e-b525-97c72e4e083f",
-    )
-    mocker.patch("garden_ai.client.GardenClient._update_datacite", return_value=None)
-    mocker.patch("garden_ai.client.local_data.put_local_pipeline", return_value=None)
-
-    pipeline_toy_example.pip_dependencies = ["tensorflow"]
-    garden_client.register_pipeline(pipeline_toy_example)
-
-    assert build_method.call_count == 0
-
-    pipeline_toy_example.pip_dependencies = ["opencv-python"]
-    garden_client.register_pipeline(pipeline_toy_example)
-
-    assert build_method.call_count == 1
-
     reqs_a = ["tensorflow", "pandas", "mlflow==2.5.0"]
     reqs_dup_a = ["tensorflow", "pandas", "mlflow==2.5.0", "pandas"]
     reqs_reorder_a = ["mlflow==2.5.0", "tensorflow", "pandas"]
@@ -94,3 +67,30 @@ def test_local_cache(mocker, garden_client, pipeline_toy_example):
         == get_cache_tag(reqs_reorder_a)
     )
     assert get_cache_tag(reqs_a) != get_cache_tag(reqs_b)
+
+    build_method = mocker.patch(
+        "garden_ai.client.build_container",
+        return_value="d1fc6d30-be1c-4ac4-a289-d87b27e84357",
+    )
+    # return value is the checksum for pip_dependencies=["tensorflow"] (implicitly also contains pandas<3 and mlflow-skinny==2.5.0)
+    mocker.patch(
+        "garden_ai.client._read_local_cache",
+        return_value={get_cache_tag(reqs_a): "04332b26-bc14-45e1-a296-482e27c72500"},
+    )
+    mocker.patch("garden_ai.client._write_local_cache", return_value=None)
+    mocker.patch(
+        "garden_ai.client.register_pipeline",
+        return_value="9f5688ac-424d-443e-b525-97c72e4e083f",
+    )
+    mocker.patch("garden_ai.client.GardenClient._update_datacite", return_value=None)
+    mocker.patch("garden_ai.client.local_data.put_local_pipeline", return_value=None)
+
+    pipeline_toy_example.pip_dependencies = reqs_a
+    garden_client.register_pipeline(pipeline_toy_example)
+
+    assert build_method.call_count == 0
+
+    pipeline_toy_example.pip_dependencies = ["opencv-python"]
+    garden_client.register_pipeline(pipeline_toy_example)
+
+    assert build_method.call_count == 1
