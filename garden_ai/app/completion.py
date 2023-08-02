f2c0247ac27e2c7b2315ff0645b1c3a274e9603d
allisondaemicke <adaem2@illinois.edu>
2023-08-02 10:27:13 -0500

    Completion helpers

--- garden_ai/app/completion.py
+++ garden_ai/app/completion.py
@@ -4,7 +4,7 @@ from garden_ai import local_data
 def complete_model(incomplete: str):
     completion = []
     models = local_data.get_all_local_models()
-    all_model_names = [model.full_name for model in models]
+    all_model_names = [model.full_name for model in models] if models else []
     if not incomplete:
         return all_model_names
     for name in all_model_names:
@@ -16,7 +16,9 @@ def complete_model(incomplete: str):
 def complete_pipeline(incomplete: str):
     completion = []
     pipelines = local_data.get_all_local_pipelines()
-    all_pipelines = [(pipeline.doi, pipeline.title) for pipeline in pipelines]
+    all_pipelines = (
+        [(pipeline.doi, pipeline.title) for pipeline in pipelines] if pipelines else []
+    )
     if not incomplete:
         return all_pipelines
     for name, help_text in all_pipelines:
@@ -28,7 +30,7 @@ def complete_pipeline(incomplete: str):
 def complete_garden(incomplete: str):
     completion = []
     gardens = local_data.get_all_local_gardens()
-    all_gardens = [(garden.doi, garden.title) for garden in gardens]
+    all_gardens = [(garden.doi, garden.title) for garden in gardens] if gardens else []
     if not incomplete:
         return all_gardens
     for name, help_text in all_gardens:
