36f13f6e4002c8f06b8265ad3e9e8d4e9635a72d
MaxTuecke <maxtuecke@gmail.com>
2023-10-26 14:33:29 -0500

    premade images

--- garden_ai/constants.py
+++ garden_ai/constants.py
@@ -20,3 +20,23 @@ class GardenConstants:
         else _DEV_SEARCH_INDEX
     )
     DLHUB_ENDPOINT = "86a47061-f3d9-44f0-90dc-56ddc642c000"
+
+    PREMADE_IMAGES = {
+        "3.8-base": "maxtuecke/garden-ai:python-3.8-jupyter",
+        "3.9-base": "maxtuecke/garden-ai:python-3.9-jupyter",
+        "3.10-base": "maxtuecke/garden-ai:python-3.10-jupyter",
+        "3.11-base": "maxtuecke/garden-ai:python-3.11-jupyter",
+        "3.8-sklearn": "maxtuecke/garden-ai:python-3.8-jupyter-sklearn",
+        "3.9-sklearn": "maxtuecke/garden-ai:python-3.9-jupyter-sklearn",
+        "3.10-sklearn": "maxtuecke/garden-ai:python-3.10-jupyter-sklearn",
+        "3.11-sklearn": "maxtuecke/garden-ai:python-3.11-jupyter-sklearn",
+        "3.8-tensorflow": "maxtuecke/garden-ai:python-3.8-jupyter-tensorflow",
+        "3.9-tensorflow": "maxtuecke/garden-ai:python-3.9-jupyter-tensorflow",
+        "3.10-tensorflow": "maxtuecke/garden-ai:python-3.10-jupyter-tensorflow",
+        "3.11-tensorflow": "maxtuecke/garden-ai:python-3.11-jupyter-tensorflow",
+        "3.8-torch": "maxtuecke/garden-ai:python-3.8-jupyter-torch",
+        "3.9-torch": "maxtuecke/garden-ai:python-3.9-jupyter-torch",
+        "3.10-torch": "maxtuecke/garden-ai:python-3.10-jupyter-torch",
+        "3.11-torch": "maxtuecke/garden-ai:python-3.11-jupyter-torch",
+        "3.10-all-extras": "maxtuecke/garden-ai:python-3.10-jupyter-all",
+    }
