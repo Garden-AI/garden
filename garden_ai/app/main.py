ef4f13bb09602329bdb092ae1b5ac6817fe7f42f
isaac-darling <61167692+isaac-darling@users.noreply.github.com>
2023-11-07 15:39:10 -0600

    add half-baked setup command

--- garden_ai/app/main.py
+++ garden_ai/app/main.py
@@ -1,9 +1,11 @@
 import logging
+import platform
 from typing import Optional
 
-import rich
+import docker
 import typer
 
+from garden_ai.app.console import console
 from garden_ai.app.garden import garden_app
 from garden_ai.app.pipeline import pipeline_app
 from garden_ai.app.notebook import notebook_app
@@ -23,7 +25,7 @@ app.add_typer(notebook_app)
 def show_version(show: bool):
     """Display the installed version and quit."""
     if show:
-        rich.print(f"garden-ai {__version__}")
+        console.print(f"garden-ai {__version__}")
         raise typer.Exit()
 
 
@@ -37,3 +39,36 @@ def main_info(
     🌱 Hello, Garden 🌱
     """
     pass
+
+
+@app.command(no_args_is_help=True)
+def setup(
+    repo: str = typer.Argument(
+        ...,
+        help=("Name of a Repository you can push docker images to."),
+    ),
+):
+    """Perform checks on the Docker Desktop configuration.
+    Report all errors encountered with possible fixes."""
+    checks_passed = True
+
+    try:
+        client = docker.from_env()
+        res = client.login("gardenai")
+        print(res)
+
+    except docker.errors.DockerException:
+        checks_passed = False
+        console.print("[bold red]Setup found an issue with Docker Desktop!")
+        console.print("[bold red]Please ensure you have Docker Desktop running.")
+
+        # an issue Max discovered on Mac systems (#318)
+        if platform.system() == "Darwin":
+            console.print(
+                "[bold red]And please ensure the toggle is set to 'on' at "
+                "Settings > Advanced > Allow the default Docker socket to be used."
+            )
+
+    if checks_passed:
+        # the double space is needed to not overlap characters
+        console.print("[bold green3]✔️  Setup passed all checks!")
