4bead9fd40aa752ad2523a552df7acdaee97826d
Will Engler <Engler.Will@gmail.com>
2023-11-20 10:27:01 -0600

    Enable imports from HF repos

--- garden_ai/model_connectors/hugging_face.py
+++ garden_ai/model_connectors/hugging_face.py
@@ -1,14 +1,18 @@
 import huggingface_hub as hfh  # type: ignore
 from garden_ai.mlmodel import ModelMetadata
 import os
+import sys
 
 
 class HFConnector:
-    def __init__(self, repo_id: str, revision=None, local_dir=None):
+    def __init__(
+        self, repo_id: str, revision=None, local_dir=None, enable_imports=True
+    ):
         self.repo_id = repo_id
         self.revision = revision
         self.local_dir = local_dir or "hf_model"
         self.model_card = hfh.ModelCard.load(repo_id)
+        self.enable_imports = enable_imports
         self.metadata = ModelMetadata(
             model_identifier=self.repo_id,
             model_repository="Hugging Face",
@@ -19,6 +23,8 @@ class HFConnector:
         if not os.path.exists(self.local_dir):
             os.mkdir(self.local_dir)
         hfh.snapshot_download(self.repo_id, local_dir=self.local_dir)
+        if self.enable_imports:
+            sys.path.append(self.local_dir)
         return self.local_dir
 
     def _repr_html_(self):
