4c5abaa0e64b7f5e79e74a9255545816ad8b3f96
Isaac Darling <61167692+isaac-darling@users.noreply.github.com>
2023-12-15 16:49:57 -0600

    add user operations to garden cli

--- garden_ai/app/main.py
+++ garden_ai/app/main.py
@@ -7,7 +7,9 @@ import typer
 from garden_ai.app.garden import garden_app
 from garden_ai.app.entrypoint import entrypoint_app
 from garden_ai.app.notebook import notebook_app
+from garden_ai import GardenClient
 from garden_ai._version import __version__
+from garden_ai.local_data import _get_user_email
 
 logger = logging.getLogger()
 
@@ -27,11 +29,37 @@ def show_version(show: bool):
         raise typer.Exit()
 
 
+def user_operations(command: Optional[str]):
+    """Handle whoami, login, or logout command then quit."""
+    if command == "whoami":
+        user = _get_user_email()
+        if user != "unknown":
+            rich.print(user)
+        else:
+            # undefined behavior if the user is not actually logged out
+            GardenClient()
+    elif command == "login":
+        # forces the login flow when logged out
+        GardenClient()
+    elif command == "logout":
+        # assumes user is logged in, seems like a safe assumption
+        # otherwise: undefined behavior
+        GardenClient().auth_key_store.clear_garden_data()
+    else:
+        if command is not None:
+            rich.print(f"Unknown command: {command}.")
+    # quit
+    raise typer.Exit()
+
+
 @app.callback()
 def main_info(
+    user_op: Optional[str] = typer.Argument(
+        None, callback=user_operations, is_eager=True
+    ),
     version: Optional[bool] = typer.Option(
         None, "--version", callback=show_version, is_eager=True
-    )
+    ),
 ):
     """
     🌱 Hello, Garden 🌱
