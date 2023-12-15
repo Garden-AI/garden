887c97b2976438a7f2d0184fdb1271b04a021c02
Isaac Darling <61167692+isaac-darling@users.noreply.github.com>
2023-12-15 17:30:28 -0600

    revert added method

--- garden_ai/garden_file_adapter.py
+++ garden_ai/garden_file_adapter.py
@@ -25,9 +25,5 @@ class GardenFileAdapter(FileAdapter):
     def on_refresh(self, token_response: OAuthTokenResponse) -> None:
         self.store(token_response)
 
-    def clear_garden_data(self) -> None:
-        # silently ignores the case where the file is already gone
-        Path.unlink(Path(self.garden_key_store.filename), missing_ok=True)
-
     def file_exists(self) -> bool:
         return self.garden_key_store.file_exists()
