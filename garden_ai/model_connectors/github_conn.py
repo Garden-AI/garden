190b81aaa38aa468c3f5852f086ec1e01b3700f9
Steve-Barnard <steve@coterieinsurance.com>
2024-01-22 13:54:43 -0600

    updates per PR #391

--- garden_ai/model_connectors/github_conn.py
+++ garden_ai/model_connectors/github_conn.py
@@ -9,20 +9,24 @@ import requests
 
 class GitHubConnector:
     def __init__(
-        self, repo_url: str, revision=None, local_dir=None, enable_imports=True
+        self,
+        repo_url: str,
+        branch="main",
+        local_dir=None,
+        enable_imports=True,
     ):
         self.repo_url = repo_url
-        self.revision = revision
+        self.branch = branch
         self.local_dir = local_dir or "gh_model"
         self.enable_imports = enable_imports
         self.metadata = ModelMetadata(
             model_identifier=self.repo_url,
             model_repository="GitHub",
-            model_version=self.revision,
+            model_version=self.branch,
         )
         try:
             repo_url_split = repo_url.split("/")
-            readme_url = f"https://raw.githubusercontent.com/{repo_url_split[-2]}/{repo_url_split[-1]}/main/README.md"
+            readme_url = f"https://raw.githubusercontent.com/{repo_url_split[-2]}/{repo_url_split[-1]}/{self.branch}/README.md"
             self.read_me = requests.get(readme_url).text
         except HTTPError:
             self.read_me = ""
@@ -30,11 +34,11 @@ class GitHubConnector:
             self.read_me = ""
 
     @trackcalls
-    def stage(self) -> str:  # TODO Review this
+    def stage(self) -> str:
         if not os.path.exists(self.local_dir):
             os.mkdir(self.local_dir)
 
-        Repo.clone_from(f"{self.repo_url}.git", self.local_dir)
+        Repo.clone_from(f"{self.repo_url}.git", self.local_dir, branch=self.branch)
 
         if self.enable_imports:
             sys.path.append(self.local_dir)
