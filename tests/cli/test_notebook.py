872d24ed3876b7fe5b8a6082ab19be5cf21a2c08
Will Engler <Engler.Will@gmail.com>
2024-02-05 12:34:43 -0600

    add new test

--- tests/cli/test_notebook.py
+++ tests/cli/test_notebook.py
@@ -4,10 +4,14 @@ from typer import Exit
 from docker.errors import DockerException  # type: ignore
 
 from garden_ai.app.notebook import DockerClientSession
-from garden_ai.containers import DockerStartFailure
+from garden_ai.containers import (
+    DockerStartFailure,
+    DockerBuildFailure,
+    DockerPreBuildFailure,
+)
 
 
-def test_docker_client_session():
+def test_docker_client_session_start_failure():
     mock_docker_exception = Mock(spec=DockerException)
     docker_start_failure = DockerStartFailure(
         mock_docker_exception, "turn it off and back on again"
@@ -22,3 +26,21 @@ def test_docker_client_session():
                 # Just needed to trigger __enter__
                 pass
     assert exc_info.value.exit_code == 1, "Exit code is not 1."
+
+
+@pytest.mark.parametrize(
+    "build_error_class", [DockerBuildFailure, DockerPreBuildFailure]
+)
+def test_docker_client_session_build_failure(capsys, build_error_class):
+    build_error = build_error_class(reason="bad notebook", build_log=["a", "b", "c"])
+
+    # Does it trigger a clean typer.Exit when Docker build fails?
+    with pytest.raises(Exit) as exc_info:
+        with patch("garden_ai.app.notebook.get_docker_client"):
+            with DockerClientSession():
+                raise build_error
+    assert exc_info.value.exit_code == 1, "Exit code is not 1."
+
+    # And does it print the expected error message?
+    captured = capsys.readouterr()
+    assert "Fatal Docker build error:" in captured.out, "Error message not in output."
