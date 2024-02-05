397f216e6a5233647b689b6c8e46ebb6e21b7320
Will Engler <Engler.Will@gmail.com>
2024-02-05 11:07:21 -0600

    Specify verbosity for all client sessions

--- garden_ai/app/notebook.py
+++ garden_ai/app/notebook.py
@@ -214,7 +214,7 @@ def start(
         f"If you start this notebook again from the same folder, it will use this image by default."
     )
 
-    with DockerClientSession() as docker_client:
+    with DockerClientSession(verbose=False) as docker_client:
         # pre-bake local image with garden-ai and additional user requirements
         local_base_image_id = build_image_with_dependencies(
             docker_client, base_image_uri, requirements_path
