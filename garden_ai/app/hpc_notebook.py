9c99efc4ebd1c24e809e317e665eb428c121d40d
jisenli2 <jisenli2@illinois.edu>
2024-06-21 13:34:16 -0500

    Fix issue #484: Set up the CLI for hpc-notebook.

---
+++ garden_ai/app/hpc_notebook.py
@@ -0,0 +1,28 @@
+import logging
+import typer
+
+logger = logging.getLogger()
+
+hpc_notebook_app = typer.Typer(name="hpc-notebook")
+
+
+@hpc_notebook_app.callback(no_args_is_help=True)
+def hpc_notebook():
+    """sub-commands for editing and publishing from sandboxed notebooks in HPC."""
+    pass
+
+
+@hpc_notebook_app.command()
+def start():
+    """Open a notebook file in HPC."""
+    print(f"🚧🌱🚧 Under Construction 🚧🌱🚧")
+
+
+@hpc_notebook_app.command()
+def publish():
+    """Publish your hpc-notebook."""
+    print(f"🚧🌱🚧 Under Construction 🚧🌱🚧")
+
+
+if __name__ == "__main__":
+    hpc_notebook_app()
