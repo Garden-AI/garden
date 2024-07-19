a7ba04735ee25987c640116b04ce894176b6253b
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-07-19 12:27:19 -0500

    flip the _IS_DISABLED switches

--- garden_ai/local_data.py
+++ garden_ai/local_data.py
@@ -1,11 +1,4 @@
-import os
-from pathlib import Path
-
-from dotenv import load_dotenv
-
-dotenv_path = Path(f"{__file__}/../..").resolve() / ".env"
-load_dotenv(dotenv_path=dotenv_path)
-_IS_DISABLED = bool(os.getenv("GARDEN_DISABLE_LOCAL_DATA"))
+_IS_DISABLED = True
 
 
 import json  # noqa: E402
