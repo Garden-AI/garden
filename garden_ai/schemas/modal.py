6addb2fe7f12915c16121752b7a7deb4692b60bb
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-10-11 15:49:21 -0500

    update schemas

--- garden_ai/schemas/modal.py
+++ garden_ai/schemas/modal.py
@@ -1,11 +1,37 @@
-from .schema_utils import B64Bytes
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from .entrypoint import (
+    DatasetMetadata,
+    ModelMetadata,
+    PaperMetadata,
+    RepositoryMetadata,
+)
+from .schema_utils import B64Bytes, UniqueList
 
 
 class ModalFunctionMetadata(BaseModel):
-    app_name: str
+    # Identifiers
+    id: int
+    doi: str | None = None
+
+    # DataCite Metadata
+    title: str
+    description: str | None = ""
+    year: str
+
+    # Function Metadata
+    is_archived: bool = False
     function_name: str
-    # TODO other fields we'll want to persist
+    function_text: str
+
+    authors: UniqueList[str] = Field(default_factory=list)
+    tags: UniqueList[str] = Field(default_factory=list)
+    test_functions: list[str] = Field(default_factory=list)
+
+    models: list[ModelMetadata] = Field(default_factory=list)
+    repositories: list[RepositoryMetadata] = Field(default_factory=list)
+    papers: list[PaperMetadata] = Field(default_factory=list)
+    datasets: list[DatasetMetadata] = Field(default_factory=list)
 
 
 class _ModalGenericResult(BaseModel):
@@ -21,8 +47,7 @@ class _ModalGenericResult(BaseModel):
 
 
 class ModalInvocationRequest(BaseModel):
-    app_name: str
-    function_name: str
+    function_id: int
     args_kwargs_serialized: B64Bytes
 
 
